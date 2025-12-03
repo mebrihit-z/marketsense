d1f0cc6133ad06ce9e551f6cb58e64d99dd1aa57
mebrihit-z <mebrihit@cxstudios.io>
2025-12-03 15:58:29 -0500

    css changes

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoadingSpinnerComponent } from '../../shared/components/loading-spinner/loading-spinner.component';
 import { FiltersBarComponent } from '../../shared/components/filters/filters-bar/filters-bar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, LoadingSpinnerComponent, FiltersBarComponent],
+  imports: [CommonModule, FiltersBarComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
