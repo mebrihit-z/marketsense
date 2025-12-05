6b3cfa00da4668adcf11c1f8c44190ba2676e7fb
mebrihit-z <mebrihit@cxstudios.io>
2025-12-05 11:02:12 -0500

    chip fix

--- src/app/shared/components/filters/selected-filter-chip/selected-filter-chip.component.ts
+++ src/app/shared/components/filters/selected-filter-chip/selected-filter-chip.component.ts
@@ -10,4 +10,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class SelectedFilterChipComponent {
   @Input() label = '';
   @Output() remove = new EventEmitter<void>();
+
+  get labelName(): string {
+    const colonIndex = this.label.indexOf(':');
+    return colonIndex !== -1 ? this.label.substring(0, colonIndex + 1) : this.label;
+  }
+
+  get labelValue(): string {
+    const colonIndex = this.label.indexOf(':');
+    return colonIndex !== -1 ? this.label.substring(colonIndex + 1).trim() : '';
+  }
 }
