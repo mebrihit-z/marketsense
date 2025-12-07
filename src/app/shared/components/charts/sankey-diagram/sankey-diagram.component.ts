16a5cfa2c6fe4efefcc2093d47bf4fb91814389a
mebrihit-z <mebrihit@cxstudios.io>
2025-12-06 19:27:08 -0500

    using components on the page

--- src/app/shared/components/charts/sankey-diagram/sankey-diagram.component.ts
+++ src/app/shared/components/charts/sankey-diagram/sankey-diagram.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sankey-diagram',
+  standalone: true,
   imports: [],
   templateUrl: './sankey-diagram.component.html',
   styleUrl: './sankey-diagram.component.scss'
