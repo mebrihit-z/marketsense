754501727625087af201ba5c55d2b58fccca820d
mebrihit-z <mebrihit@cxstudios.io>
2025-12-08 10:58:32 -0500

    removing pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,9 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { AboutComponent } from './pages/about/about.component';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent,
   },
-  { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,21 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 
 // Page Components
-import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'about', component: AboutComponent }
 ];
 
 @NgModule({
   declarations: [
-    HomeComponent,
     DashboardComponent,
-    AboutComponent
   ],
   imports: [
     SharedModule,
