975b9d1a1b5f88e3b2ca5b9c07bad1e7ba17001a
mebrihit-z <mebrihit@cxstudios.io>
2026-04-05 00:23:20 -0400

    some ui changes

--- src/app/shared/components/market-flows-carousel/market-flow-detail-modal/market-flow-detail-modal.component.ts
+++ src/app/shared/components/market-flows-carousel/market-flow-detail-modal/market-flow-detail-modal.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import type { MarketFlowCard } from '../market-flow-card/market-flow-card.component';
